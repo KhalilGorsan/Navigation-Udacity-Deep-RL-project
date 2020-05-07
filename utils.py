5c9d23bf9c8e642cd2bcc227b2c57a7e933a98eb
Khalil Gorsan Mestiri <khalilgorsan@gmail.com>
2020-05-08 00:33:03 +0100

    Add utility functions

---
+++ utils.py
@@ -0,0 +1,11 @@
+import yaml
+from pathlib import Path
+
+
+
+def extract_configs(filename: Path) -> dict:
+    """Utility function serves to read YAML files and load them into python dict.
+    """
+    with open(filename) as file:
+        content = yaml.load(file, Loader=yaml.FullLoader)
+    return content
