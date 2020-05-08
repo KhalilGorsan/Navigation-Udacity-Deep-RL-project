08899d6591d76c161c36f8b288d5676088c081c9
Khalil Gorsan Mestiri <khalilgorsan@gmail.com>
2020-05-08 23:55:24 +0100

    Add a method to close the env

--- core.py
+++ core.py
@@ -21,8 +21,10 @@ class BananaWrapper:
         next_state = env_info.vector_observations[0]
         reward = env_info.rewards[0]
         done = env_info.local_done[0]
-
         return next_state, reward, done
+
+    def close(self):
+        self.env.close()
 
     @property
     def action_size(self):
@@ -31,3 +33,4 @@ class BananaWrapper:
     @property
     def observation_size(self):
         return self.brain.vector_observation_space_size
+
