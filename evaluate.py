3c5ba8446c92b90583cd7bb96407b9090cce38fe
Khalil Gorsan Mestiri <khalilgorsan@gmail.com>
2020-05-08 23:56:11 +0100

    Evaluate the trained models

---
+++ evaluate.py
@@ -0,0 +1,49 @@
+# load the weights from file
+import torch
+from dqn_agent import Agent
+from utils import extract_configs
+from core import BananaWrapper
+from absl import app, flags
+import time
+
+flags.DEFINE_multi_string(
+    "eval_config", None, "Filename containing the config for dqn agent."
+)
+FLAGS = flags.FLAGS
+
+
+def main(unused_argv):
+    del unused_argv
+
+    configs = extract_configs(*FLAGS.eval_config)
+    # instantiate the Banana env
+    env = BananaWrapper(file_name="./Banana")
+    state_size = env.observation_size
+    action_size = env.action_size
+    # instantiate agent object
+    agent = Agent(state_size=state_size, action_size=action_size, configs=configs)
+
+    # load trained model
+    agent.qnetwork_local.load_state_dict(
+        torch.load("results/checkpoints/DoubleDQN.pth")
+    )
+
+    horizon = 1000
+    episodes = 5
+    for _ in range(episodes):
+        state = env.reset()
+        for _ in range(horizon):
+            # Perform action given the trained policy
+            action = agent.act(state)
+            next_state, reward, done = env.step(action)
+            time.sleep(0.05)
+            if done:
+                break
+            state = next_state
+
+    # close env
+    env.close()
+
+if __name__ == "__main__":
+    flags.mark_flag_as_required("eval_config")
+    app.run(main)
